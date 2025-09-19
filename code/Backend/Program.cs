905535fd7eb2bafe1fbaebb410b0795c5a173481
AntoinePiguet <pn01kbc@eduvaud.ch>
2025-09-19 14:14:16 +0000

    Create Backend : creation du backend du projet ainsi que installation des dépendances du projet

---
+++ code/Backend/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class ApiCaller
+{
+    private static readonly HttpClient finnhub = new HttpClient();
+    public static async Task CallApiAsync(string apiUrl)
+    {
+        try 
+        {
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("API answer :"  + responseBody);
+        }
+        catch(HttpRequestException e)
+        {
+            Console.WriteLine("Erreur lors de l'appel API : " + e.Message);
+        }
+    }
+}
